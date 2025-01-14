3c2f30f55c6534b94f1acdb3d58a23ffc023c926
u-games <ugames.uparty@gmail.com>
2025-01-14 13:09:34 +0100

    Update user.js

--- user.js
+++ user.js
@@ -1,8 +1,6 @@
-function guardarNombreYRedirigir() {
+function Redirigir() {
     const nombre = document.getElementById('input-nombre').value.trim();
     if (nombre) {
-        // Guardar el nombre en el almacenamiento local
-        localStorage.setItem('nombreJugador', nombre);
         // Redirigir a la página de la historia
         window.location.href = 'juego.html';
     }
